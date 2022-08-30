ea59140323943087bdf827da6022e866c1b0df94
wynss <toniaxelsson@gmail.com>
2022-08-30 10:30:47 +0200

    split components into multiple files (#2)
    small various fixes

---
+++ dashy/components/layout.py
@@ -0,0 +1,87 @@
+from typing import Optional, Union
+
+from dash.development.base_component import Component
+from dash import html
+import dash_bootstrap_components as dbc
+
+from .helpers import get_margin_class, get_padding_class
+
+
+def container(
+    children: list = None, 
+    fluid: bool = True, 
+    margin: int = 1,
+    margin_right: int = None,
+    margin_left: int = None,
+    margin_top: int = None,
+    margin_bottom: int = None,
+    padding: int = 0, 
+    padding_right: int = None,
+    padding_left: int = None,
+    padding_top: int = None,
+    padding_bottom: int = None,
+    **kwargs
+) -> dbc.Container:
+    kwargs = {
+        'className': (
+            f'{get_margin_class(margin, margin_top, margin_bottom, margin_left, margin_right)} '
+            f'{get_padding_class(padding, padding_top, padding_bottom, padding_left, padding_right)}'
+        ),
+        **kwargs
+    }
+    return dbc.Container(children, fluid=fluid, **kwargs)
+
+
+def row(
+    children: Union[list, str, Component],
+    margin: int = 1,
+    margin_right: int = None,
+    margin_left: int = None,
+    margin_top: int = None,
+    margin_bottom: int = None,
+    padding: int = 0, 
+    padding_right: int = None,
+    padding_left: int = None,
+    padding_top: int = None,
+    padding_bottom: int = None,
+    **kwargs
+) -> dbc.Row:
+    kwargs = {
+        'className': (
+            f'{get_margin_class(margin, margin_top, margin_bottom, margin_left, margin_right)} '
+            f'{get_padding_class(padding, padding_top, padding_bottom, padding_left, padding_right)} '
+            f'row-auto d-flex align-items-center'
+        ),
+        **kwargs
+    }
+    return dbc.Row(children, **kwargs)
+
+
+def col(
+    children: Union[list, str, Component],
+    margin: int = 1,
+    margin_right: Optional[int] = None,
+    margin_left: Optional[int] = None,
+    margin_top: Optional[int] = None,
+    margin_bottom: Optional[int] = None,
+    padding: int = 0, 
+    padding_right: int = None,
+    padding_left: int = None,
+    padding_top: int = None,
+    padding_bottom: int = None,
+    **kwargs
+) -> dbc.Col:
+    kwargs = {
+        'className': (
+            f'{get_margin_class(margin, margin_top, margin_bottom, margin_left, margin_right)} '
+            f'{get_padding_class(padding, padding_top, padding_bottom, padding_left, padding_right)} '
+            f'col-auto d-flex flex-column align-items-start'
+        ),
+        **kwargs
+    }
+    return dbc.Col(children, **kwargs)
+
+
+def div(children: Optional[Union[list, str, int, float]] = None, id: str = None, hidden: bool = False):
+    kwargs = {k: v for k, v in locals().items() if v is not None}
+    return html.Div(**kwargs)
--- examples/overview.py
+++ examples/overview.py
@@ -9,13 +9,19 @@ import plotly.express as px
 layout = [
     dc.navbar('Dashy Overview!', dark=True, buttons=['Home', 'Overview', 'Analysis']),
     dc.tabs(id='tabs', labels=['Plots', 'Buttons', 'Tab3'], content_id='my-content'),
-    dc.row([
-        dc.card('my-card', "This is a card", "this is some text"), 
-        dc.card('my-card-2', "This is a card", "this is some text")
+    dc.container([
+        dc.row([
+            dc.card('my-card-1', "This is a card", "this is some text"),
+            dc.card('my-card-2', "This is a card", "this is some text")
+        ]),
+        dc.col([
+            dc.card('my-card-3', "This is a card", "this is some text"),
+            dc.card('my-card-4', "This is a card", "this is some text")
+        ])
     ])
 ]
 
-app = dy.create_app('MyApp', layout, theme=dy.themes.FLATLY, suppress_callback_exceptions=True)
+app = dy.create_app('MyApp', layout, theme=dy.themes.QUARTZ, suppress_callback_exceptions=True)
 
 
 @app.cb(('tabs', 'active_tab'), ('my-content', 'children'))
@@ -26,19 +32,25 @@ def switch_tabs(tab_id: str):
                 dc.row([
                     dc.graph("my-graph-1", figure=px.line(y=np.arange(10))), 
                     dc.graph("my-graph-2", figure=px.scatter(y=2*np.linspace(-10, 10, 100)**2 + 1))
-                ])
+                ], margin=0)
             ])
         case 'buttons':
-            return dc.container(
+            return dc.container([
                 dc.row([
                     dc.button(text="Button", id='btn-1'),
-                    dc.button(text="Button with a popover", id='btn-2', popover_header="This is a popover"),
-                    dc.button(text="Button with a popover and delay", id='btn-3',
-                              popover_body="This is a popover a bit delayed!", popover_delay=500,
-                              popover_placement=dc.Placement.TOP),
+                    dc.button(text="Popover", id='btn-2', popover_header="This is a popover", color=dc.Color.SECONDARY),
+                    dc.button(text="Popover and delay", id='btn-3',
+                              popover_body="Below and a bit delayed!", popover_delay=500,
+                              popover_placement=dc.Placement.BOTTOM),
                     dc.button("Button with a spinner", id='btn-4', spinner_div_id='btn-spinner-div')
-                ])
-            )
+                ], margin=0),
+                dc.row([
+                    dc.dropdown('Dropdown', width=300),
+                    dc.checklist(id='checklist', header="Some checks", labels=['Check 1', 'Check 2', 'Check 3'], initial='Check 1'),
+                    dc.radios(id='radios', header="Some Radios", labels=['Radio 1', 'Radio 2', 'Radio 3']),
+                    dc.inputs(ids=['input-1', 'input-2', 'input-3'], titles=['Input 1', 'Input 2', 'Input 3'], input_type=[dc.InputType.TEXT, dc.InputType.NUMBER, dc.InputType.RANGE])
+                ], margin=0)
+            ], margin=0)
         case _:
             return dc.div(f'Hello from {tab_id}')
 
