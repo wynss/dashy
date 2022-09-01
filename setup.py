3a2658950e729309aa4f77f31d4dd6ab92712407
wynss <toniaxelsson@gmail.com>
2022-09-01 12:24:21 +0200

    refactor and add pyproject.toml (#13)
    Also add LICENSE file

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup
 
-setup(
-    name='dashy',
-    version='0.1dev',
-    author='Johan Doe',
-    author_email='some.person@email.com',
-    packages=find_packages(),
-    install_requires=[
-        'dash >= 2.6.1',
-        'dash-bootstrap-components >= 1.2.1',
-        'libsass >= 0.21.0'
-    ],
-    include_package_data=True
-)
+setup()
